512e51c3689bc36659acfca8b560be8c51c255f8
Iyathurai Iyngaran <dev@iyngaran.info>
2021-02-12 17:06:19 +0530

    wip

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -17,7 +17,6 @@ class ExampleTest extends TestCase
     public function true_is_true()
     {
 
-        /*
         $category = Category::factory()
             ->child()
             ->create();
@@ -27,8 +26,7 @@ class ExampleTest extends TestCase
             ->published()
             ->reviewed()
             ->create();
-        dd($post);
-        */
+
 
         auth()->login(User::create([
             'name' => 'Iyngaran',
@@ -36,40 +34,6 @@ class ExampleTest extends TestCase
             'password' => 'password!',
         ]));
 
-
-        $category = \Iyngaran\Category\Models\Category::create([
-            'name' => ucfirst($this->faker->word),
-            'status' => (string)$this->faker->randomElement([0,1]),
-            'image' => $this->faker->word.".png",
-            'small_description' => $this->faker->sentence(10),
-            'detail_description' => $this->faker->paragraph(3),
-        ]);
-
-        $sub_category = \Iyngaran\Category\Models\Category::create([
-            'name' => ucfirst($this->faker->word),
-            'status' => (string)$this->faker->randomElement([0,1]),
-            'image' => $this->faker->word.".png",
-            'small_description' => $this->faker->sentence(10),
-            'detail_description' => $this->faker->paragraph(3),
-            'parent_id' => $category->id,
-        ]);
-
-        $response = $this->post(
-            'api/post/store',
-            [
-                'title' => $this->faker->text(150),
-                'for' => 'sale',
-                'condition' => 1,
-                'short_description' => null,
-                'detail_description' => null,
-                'price' => $this->faker->randomFloat(2),
-                'currency' => $this->faker->currencyCode,
-                'negotiable' => 1,
-                'category' => $category->id,
-                'sub_category' => $sub_category->id,
-                'status' => 'Published',
-            ]
-        );
-        $response->assertStatus(201);
+        $this->assertTrue(true);
     }
 }
---
+++ tests/Http/Controllers/Api/PostControllerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace Iyngaran\Advertiser\Tests\Http\Controllers\Api;
+
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Iyngaran\Advertiser\Tests\Models\User;
+use Iyngaran\Advertiser\Tests\TestCase;
+use Iyngaran\Category\Models\Category;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    private function mockPostData(): array
+    {
+        $category = Category::create([
+            'name' => ucfirst($this->faker->word),
+            'status' => (string)$this->faker->randomElement([0, 1]),
+            'image' => $this->faker->word.".png",
+            'small_description' => $this->faker->sentence(10),
+            'detail_description' => $this->faker->paragraph(3),
+        ]);
+
+        $sub_category = Category::create([
+            'name' => ucfirst($this->faker->word),
+            'status' => (string)$this->faker->randomElement([0, 1]),
+            'image' => $this->faker->word.".png",
+            'small_description' => $this->faker->sentence(10),
+            'detail_description' => $this->faker->paragraph(3),
+            'parent_id' => $category->id,
+        ]);
+
+
+        return [
+            'title' => $this->faker->text(150),
+            'for' => 'sale',
+            'condition' => 1,
+            'short_description' => null,
+            'detail_description' => null,
+            'price' => $this->faker->randomFloat(2),
+            'currency' => $this->faker->currencyCode,
+            'negotiable' => 1,
+            'category' => $category->id,
+            'sub_category' => $sub_category->id,
+            'status' => 'Published',
+        ];
+    }
+
+    /** @test */
+    public function a_post_can_be_created()
+    {
+        auth()->login(User::create([
+            'name' => 'Iyngaran',
+            'email' => 'Iyngaran55@yahoo.com',
+            'password' => 'password!',
+        ]));
+
+        $response = $this->post(
+            'api/post/store',
+            $this->mockPostData()
+        );
+        $response->assertStatus(201);
+    }
+}
