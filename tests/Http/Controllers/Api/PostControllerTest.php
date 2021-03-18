995a725da93793be7373e795823df9973113aee1
Iyathurai Iyngaran <dev@iyngaran.info>
2021-03-18 12:38:06 +0530

    wip

--- tests/Http/Controllers/Api/PostControllerTest.php
+++ tests/Http/Controllers/Api/PostControllerTest.php
@@ -71,6 +71,7 @@ class PostControllerTest extends TestCase
         Post::factory()->count(35)->create();
 
         $response = $this->get('api/classified-advertiser/post?page=2&order-by=title&order-in=ASC&belongs-to=5');
+        dd($response);
         $response->assertStatus(200);
     }
 
