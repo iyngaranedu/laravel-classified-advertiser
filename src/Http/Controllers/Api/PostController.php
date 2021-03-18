54e5451d798063d4b2f84274b06c61dc5a63dcab
Iyathurai Iyngaran <dev@iyngaran.info>
2021-03-18 12:48:31 +0530

    wip

--- src/Http/Controllers/Api/PostController.php
+++ src/Http/Controllers/Api/PostController.php
@@ -15,6 +15,7 @@ use Iyngaran\Advertiser\Http\Requests\PostShowRequest;
 use Iyngaran\Advertiser\Http\Requests\PostStoreRequest;
 use Iyngaran\Advertiser\Http\Requests\PostUpdateRequest;
 use Iyngaran\Advertiser\Http\Resources\Post;
+use Iyngaran\Advertiser\Http\Resources\PostCollection;
 use Iyngaran\Advertiser\Repositories\PostRepositoryInterface;
 
 class PostController extends Controller
