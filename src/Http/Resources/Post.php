b68a76616936eceb77790379ad92e308217d8ab1
Iyathurai Iyngaran <dev@iyngaran.info>
2021-03-18 20:29:35 +0530

    wip

--- src/Http/Resources/Post.php
+++ src/Http/Resources/Post.php
@@ -30,11 +30,18 @@ class Post extends JsonResource
             'belongs_to' => $this->belongs_to,
             'posted_by' => $this->posted_by,
             'posted_at' => $this->posted_at,
+            'posted_at_diff_for_humans' => $this->posted_at->diffForHumans(),
             'status' => $this->status,
             'published_by' => $this->published_by,
             'published_at' => $this->published_at,
+            'published_at_diff_for_humans' => $this->published_at->diffForHumans(),
             'review_status' => $this->review_status,
             'reviewed_by' => $this->reviewed_by,
+            'reviewed_at_diff_for_humans' => $this->reviewed_at->diffForHumans(),
+            'created_at' => $this->created_at,
+            'created_at_diff_for_humans' => $this->created_at->diffForHumans(),
+            'updated_at' => $this->updated_at,
+            'updated_at_diff_for_humans' => $this->updated_at->diffForHumans(),
         ];
     }
 }
