16604df626d900e3b3e4da3e54ae7d30cf6480ab
Iyathurai Iyngaran <dev@iyngaran.info>
2021-03-21 00:59:01 +0530

    wip

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Iyngaran\Advertiser\Http\Controllers\Api\PostController;
 use Iyngaran\Advertiser\Http\Controllers\Api\PublicControllers\PostController as PublicPostController;
-
+use Iyngaran\Advertiser\Http\Controllers\Api\FileUploadController;
 
 Route::group(
     ['prefix' => 'public', 'as' => 'public.'],
@@ -19,3 +19,6 @@ Route::resource('/posts', PostController::class)->except([
     'create',
     'edit'
 ]);
+
+Route::resource('/posts-file-upload', FileUploadController::class);
+
---
+++ src/Http/Controllers/Api/FileUploadController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Iyngaran\Advertiser\Http\Controllers\Api;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class FileUploadController extends Controller
+{
+    protected $destination_path = '/storage/';
+
+    public function __invoke(Request $request)
+    {
+        try {
+            $file = $request->file('file');
+            $file_name = trim(str_replace(" ", "_", $file->getClientOriginalName()));
+            Storage::disk('public')->put($file_name, File::get($file));
+        } catch (\Exception $e) {
+            return response(['errors' => ['message' => $e->getMessage()]], 404);
+        }
+        return response()->json([
+            'path' => $file_name,
+            'message' => 'Successfully updated event media!'
+        ], 200);
+    }
+}
