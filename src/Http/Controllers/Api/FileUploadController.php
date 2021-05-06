8af1af67a6b61ed978fc42098d639b48d56e4bba
Iyathurai Iyngaran <dev@iyngaran.info>
2021-05-06 14:02:05 +0530

    wip

--- src/Http/Controllers/Api/FileUploadController.php
+++ src/Http/Controllers/Api/FileUploadController.php
@@ -19,8 +19,17 @@ class FileUploadController extends Controller
             $file = $request->file('file');
             $file_name = trim(str_replace(" ", "_", $file->getClientOriginalName()));
             Storage::disk('public')->put(config('classified-advertiser.post_image_path').$file_name, File::get($file));
-            $img = Image::make(config('classified-advertiser.post_image_path').$file_name, File::get($file))->resize(300, 200);
-            Storage::disk('public')->put(config('classified-advertiser.post_image_path').'resized-'.$file_name, $img);
+            $image_path = storage_path('app/public'.config('classified-advertiser.post_image_path'));
+            $image_sizes = config('classified-advertiser.image_sizes');
+            if ($image_sizes) {
+                foreach ($image_sizes as $key => $image_size) {
+                    if(!File::exists($image_path.$key)) {
+                        File::makeDirectory($image_path.$key);
+                    }
+                    $img = Image::make($image_path.$file_name)->resize($image_size['width'], $image_size['height']);
+                    $img->save($image_path.$key.'/'.$file_name);
+                }
+            }
         } catch (\Exception $e) {
             return response(['errors' => ['message' => $e->getMessage()]], 404);
         }
