57f03abce01a78a36f5f97d4e57fcbb9a93639cc
Iyathurai Iyngaran <dev@iyngaran.info>
2021-05-06 17:30:43 +0530

    wip

--- src/Commands/AdvertiserCommand.php
+++ src/Commands/AdvertiserCommand.php
@@ -6,12 +6,25 @@ use Illuminate\Console\Command;
 
 class AdvertiserCommand extends Command
 {
-    public $signature = 'laravel-classified-advertiser';
+    public $signature = 'iynga:resize-post-images';
 
-    public $description = 'My command';
+    public $description = 'This command will resize all the post images.';
 
+    public function resizePostImages(): bool
+    {
+        try{
+            $image_path = storage_path('app/public'.config('classified-advertiser.post_image_path'));
+            console.log($image_path);
+            return true;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage());
+        }
+    }
     public function handle()
     {
-        $this->comment('All done');
+        if ($this->resizePostImages()) {
+            return $this->info('All the post images have been resized successfully!');
+        }
+        return $this->error('Failed to resize some post images!');
     }
 }
