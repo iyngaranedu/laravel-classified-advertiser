a72cbe6d7b5125c743e63d1f27a546d395247c2d
Iyathurai Iyngaran <dev@iyngaran.info>
2021-05-06 18:08:08 +0530

    wip

--- src/Commands/AdvertiserCommand.php
+++ src/Commands/AdvertiserCommand.php
@@ -3,6 +3,9 @@
 namespace Iyngaran\Advertiser\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class AdvertiserCommand extends Command
 {
@@ -12,14 +15,50 @@ class AdvertiserCommand extends Command
 
     public function resizePostImages(): bool
     {
-        try{
+        try {
             $image_path = storage_path('app/public'.config('classified-advertiser.post_image_path'));
-            print($image_path);
+            $files = Storage::disk('public')->files(config('classified-advertiser.post_image_path'));
+            $image_sizes = config('classified-advertiser.image_sizes');
+            $bar = $this->output->createProgressBar(count($files));
+            $bar->start();
+            if ($files) {
+                foreach ($files as $file) {
+                    $file_parts = pathinfo($file);
+                    if (isset($file_parts['extension']) &&
+                        (
+                            $file_parts['extension'] == "jpg"
+                            || $file_parts['extension'] == "jpeg"
+                            || $file_parts['extension'] == "png"
+                            || $file_parts['extension'] == "gif"
+                            || $file_parts['extension'] == "webp"
+                        )
+                    ) {
+                        $file_name = basename($file);
+                        if ($image_sizes) {
+                            foreach ($image_sizes as $key => $image_size) {
+                                if (!File::exists($image_path.$key)) {
+                                    File::makeDirectory($image_path.$key);
+                                }
+                                Image::make($image_path.$file_name)
+                                    ->resize($image_size['width'], $image_size['height'], function ($constraint) {
+                                        $constraint->aspectRatio();
+                                        $constraint->upsize();
+                                    })
+                                    ->save($image_path.$key.'/'.$file_name);
+                            }
+                        }
+                        $bar->advance();
+                    }
+                }
+            }
+            $bar->finish();
             return true;
         } catch (\Exception $exception) {
-            return $this->error($exception->getMessage());
+            $this->error($exception->getMessage());
+            return false;
         }
     }
+
     public function handle()
     {
         if ($this->resizePostImages()) {
