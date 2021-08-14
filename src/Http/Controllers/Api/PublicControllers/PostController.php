8c06adfe4a4a1fbde0b2bbacd7d2d891bcbfa41b
Iynga Iyngaran Iyathurai <iynga@sas.co.nz>
2021-08-14 13:56:43 +0530

    wip

--- src/Http/Controllers/Api/PublicControllers/PostController.php
+++ src/Http/Controllers/Api/PublicControllers/PostController.php
@@ -8,13 +8,14 @@ use Illuminate\Routing\Controller;
 use Iyngaran\Advertiser\Http\Requests\PublicPostIndexRequest;
 use Iyngaran\Advertiser\Http\Requests\PublicPostShowRequest;
 use Iyngaran\Advertiser\Http\Resources\Post;
+use Iyngaran\Advertiser\Http\Resources\PostCollection;
 use Iyngaran\Advertiser\Repositories\PostRepositoryInterface;
 
 class PostController extends Controller
 {
     public function index(PublicPostIndexRequest $request, PostRepositoryInterface $post): JsonResponse
     {
-        return response()->json(Post::collection($post->search($request)));
+        return response()->json(new PostCollection($post->search($request)));
     }
 
     public function show(PublicPostShowRequest $request, PostRepositoryInterface $post, $slug): JsonResponse
